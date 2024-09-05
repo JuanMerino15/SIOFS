2ef4535433600976a4a1eb426e0664cf4e9ae2e8
Jose Juan <juanrivmerino4@gmail.com>
2024-09-04 20:58:54 -0600

    Iniciando Homepage -JJRM v1

--- siofs/src/App.js
+++ siofs/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidebar">
+        <div className="profile">
+          <div className="profile-pic"></div>
+          <h3>User Name</h3>
+          <p className="role">Ministro</p>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#inicio">Inicio</a></li>
+            <li><a href="#administracion">Administración</a></li>
+            <li><a href="#economia">Economía</a></li>
+            <li><a href="#estructura-ofs">Estructura OFS</a></li>
+            <li><a href="#accesos">Accesos</a></li>
+            <li><a href="#reportes">Reportes</a></li>
+          </ul>
+        </nav>
+        <div className="logout">
+          <a href="#cerrar-sesion">Cerrar Sesión</a>
+        </div>
+      </aside>
+      
+      <main className="content">
+  <section className="welcome-container">
+    <div className="welcome">
+      <h1>Paz y Bien</h1>
+      <p>
+        Somos una comunidad de laicos comprometidos con vivir el Evangelio 
+        según el ejemplo de San Francisco de Asís. Nuestra misión es fomentar 
+        la paz, la justicia y el cuidado de la creación, sirviendo a los 
+        necesitados y construyendo una fraternidad de amor y servicio.
+      </p>
+      <p>
+        Este sistema integral está diseñado para facilitar la gestión y 
+        coordinación de nuestras actividades, asegurando una administración 
+        eficiente y transparente.
+      </p>
+    </div>
+    <div className="image-container">
+      <img src="/images/San_Francisco.jpg" alt="San Francisco de Asís" className="right-image" />
+    </div>
+  </section>
+
+  <section className="features">
+    <h2>Con el sistema podrás:</h2>
+    <div className="feature-buttons">
+      <button>Llevar el control del hermano seglar</button>
+      <button>Gestionar las asistencias</button>
+      <button>Coordinar los procedimientos</button>
+      <button>Administrar los jornales</button>
+    </div>
+  </section>
+</main>
     </div>
   );
 }
