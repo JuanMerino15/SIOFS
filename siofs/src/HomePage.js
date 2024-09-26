4784585b969d82f14dac60830f7586313590a00a
Rodrigo <rodrigoriveramacias41@gmail.com>
2024-09-25 22:31:29 -0600

    Actualizacion Homepage

--- siofs/src/HomePage.js
+++ siofs/src/HomePage.js
@@ -5,7 +5,6 @@ import './Styles/HomePage.css';
 function HomePage() {
   return (
     <div className="HomePage">
-
       <aside className="sidebar">
         <div className="profile">
           <div className="profile-pic">
@@ -15,8 +14,6 @@ function HomePage() {
           <p className="role">Ecónomo</p>
         </div>
         <nav>
-
-
           <ul>
             <li>
               <Link to="/">
@@ -29,7 +26,7 @@ function HomePage() {
               </Link>
             </li>
             <li>
-              <Link to="/Economia">
+              <Link to="/Administracion/Hermanos/submenu_HS">
                 <img src="/images/IconoEconomia.png" alt="Economía" className="icon" /> Economía
               </Link>
             </li>
@@ -49,55 +46,11 @@ function HomePage() {
               </Link>
             </li>
           </ul>
-
-        <ul>
-
-          <li><a href="#inicio"><i class="fa-regular fa-house"></i> Inicio</a></li>
-          <li><Link to="/Administracion/Hermanos/Historial_Etapa">Administración</Link></li>
-          <li><Link to="/Administracion/Hermanos/submenu_HS"> Economía</Link></li>
-          <li><a href="#estructura-ofs"><i className="fas fa-sitemap"></i> Estructura OFS</a></li>
-          <li><a href="#accesos"><i className="fas fa-key"></i> Accesos</a></li>
-          <li><a href="#reportes"><i className="fas fa-file-alt"></i> Reportes</a></li>
-        </ul>
-
-   <li>
-      <Link to="/">
-      <img src="/images/IconoHome.png" alt="Inicio" className="icon" /> Inicio
-      </Link>
-    </li>
-   <li>
-      <Link to="/Administracion/Hermanos/Historial_Etapa">
-      <img src="/images/IconoAdministracion.png" alt="Administración" className="icon" /> Administración
-     </Link>
-  </li>
-  <li>
-      <Link to="/Economia">
-      <img src="/images/IconoEconomia.png" alt="Economía" className="icon" /> Economía
-     </Link>
-  </li>
-  <li>
-     <Link to="/EstructuraOFS">
-      <img src="/images/IconoEstructuraOFS.png" alt="Estructura OFS" className="icon" /> Estructura OFS
-      </Link>
-   </li>
-  <li>
-      <Link to="/Accesos">
-      <img src="/images/IconoAccesos.png" alt="Accesos" className="icon" /> Accesos
-      </Link>
-  </li>
-  <li>
-    <Link to="/Reportes">
-      <img src="/images/IconoReportes.png" alt="Reportes" className="icon" /> Reportes
-    </Link>
-  </li>
-</ul>
-
-
         </nav>
         <div className="logout">
-        <Link to="/Cerrar-sesion">
-                <img src="/images/IconoCerrarSesion.png" alt="cerrar-sesion" className="icon" /> Cerrar Sesión
-              </Link>
+          <Link to="/Cerrar-sesion">
+            <img src="/images/IconoCerrarSesion.png" alt="cerrar-sesion" className="icon" /> Cerrar Sesión
+          </Link>
         </div>
       </aside>
 
@@ -137,7 +90,6 @@ function HomePage() {
           </div>
         </section>
       </main>
-
     </div>
   );
 }
