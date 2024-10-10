ae73ea692f5e645082fb6ea79a7c09a6044abb8b
Jose Juan <juanrivmerino4@gmail.com>
2024-10-10 17:38:58 -0600

    configurando estilos

--- siofs/src/Administracion/Hermanos/Historial_Etapa.js
+++ siofs/src/Administracion/Hermanos/Historial_Etapa.js
@@ -10,60 +10,6 @@ function HistorialEtapas() {
   return (
     <div className="HistorialEtapas">
 
-
-      <aside className="sidebar1">
-        <div className="profile">
-          <div className="profile-pic">
-            <img src="/images/icono_admin.png" className="iconA1" alt="Icono de usuario" />
-          </div>
-          <h3>User Name</h3>
-          <p className="role">Secretario</p>
-        </div>
-        <nav>
-
-
-
-        <ul>
-   <li>
-      <Link to="/">
-      <img src="/images/IconoHome.png" alt="Inicio" className="icon" /> Inicio
-      </Link>
-    </li>
-   <li>
-      <Link to="/Administracion/Hermanos/Historial_Etapa">
-      <img src="/images/IconoAdministracion.png" alt="Administración" className="icon" /> Administración
-     </Link>
-  </li>
-  <li>
-      <Link to="/Economia">
-      <img src="/images/IconoEconomia.png" alt="Economía" className="icon" /> Economía
-     </Link>
-  </li>
-  <li>
-     <Link to="/EstructuraOFS">
-      <img src="/images/IconoEstructuraOFS.png" alt="Estructura OFS" className="icon" /> Estructura OFS
-      </Link>
-   </li>
-  <li>
-      <Link to="/Accesos">
-      <img src="/images/IconoAccesos.png" alt="Accesos" className="icon" /> Accesos
-      </Link>
-  </li>
-  <li>
-    <Link to="/Reportes">
-      <img src="/images/IconoReportes.png" alt="Reportes" className="icon" /> Reportes
-    </Link>
-  </li>
-</ul>
-        </nav>
-        <div className="logout">
-        <Link to="/Cerrar-sesion">
-                <img src="/images/IconoCerrarSesion.png" alt="cerrar-sesion" className="icon" /> Cerrar Sesión
-              </Link>
-        </div>
-      </aside>
-
-
 <Sidebar />
       
 
